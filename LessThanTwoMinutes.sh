728b884a14084e1b32531fb041c6d99d893c9796
therishitakerr <75129590+therishitakerr@users.noreply.github.com>
2024-07-18 15:26:05 +0530

    Update LessThanTwoMinutes.sh

--- LessThanTwoMinutes.sh
+++ LessThanTwoMinutes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 Folder() {
-	path=$1
-	if [[ -d "$path" ]] && [[ $path != "toberemoved" ]]; then
+	path="$1"
+	if [[ -d "$path" ]] && [[ "$path" != "toberemoved" ]]; then
 		for file in "$path/"*; do
 			Folder "$file"
 		done
 	elif [ -f "$path" ]; then
 		x=2
-		duration=$(mediainfo --Inform="General;%Duration/String3%" "$path" | sed s/[:\.]/' '/g | awk '{print $2}')
+		duration=$(mediainfo --Inform="General;%Duration/String3%" "$path" | sed s/[:\.]/' '/g | awk '{print "$2"}')
 		if [ "$duration" -lt "$x" ]; then
 			mkdir -p "toberemoved"
 			mv "$path" "toberemoved"
