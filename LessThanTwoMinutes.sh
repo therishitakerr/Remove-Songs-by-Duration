1a7f74962bb4b0ed091543151dfd1a2fca060383
therishitakerr <75129590+therishitakerr@users.noreply.github.com>
2024-08-15 19:46:28 +0530

    Update LessThanTwoMinutes.sh

--- LessThanTwoMinutes.sh
+++ LessThanTwoMinutes.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-Folder() {
-	path="$1"
-	if [[ -d "$path" ]] && [[ $path != "toberemoved" ]]; then
-		for file in "$path/"*; do
-			Folder "$file"
-		done
-	elif [ -f "$path" ]; then
-		flag=2
-		full=$(mediainfo --Inform="General;%Duration/String3%" "$path")
-		hour=$(echo "$full" | sed s/[:\.]/' '/g | awk '{print $1}')
-		min=$(echo "$full" | sed s/[:\.]/' '/g | awk '{print $2}')
-		duration=$((hour * 60 + min))
-		echo "file: $path\nDuration: $duration"
-		if [ "$duration" -lt "$flag" ]; then
-			mkdir -p "toberemoved"
-			mv "$path" "toberemoved"
-		fi
-	fi
+# Function to process files and directories
+process_folder() {
+    local path="$1"
+    local flag=2
+
+    # Check if the path is a directory
+    if [[ -d "$path" && "$path" != "$(pwd)/toberemoved" ]]; then
+        # Recursively process each file and subdirectory
+        for file in "$path"/*; do
+            process_folder "$file"
+        done
+    elif [[ -f "$path" ]]; then
+        # Process the file
+        local full
+        local hour
+        local min
+        local duration
+
+        full=$(mediainfo --Inform="General;%Duration/String3%" "$path")
+        hour=$(echo "$full" | sed 's/[:.]//g' | awk '{print $1}')
+        min=$(echo "$full" | sed 's/[:.]//g' | awk '{print $2}')
+        duration=$((hour * 60 + min))
+
+        echo -e "file: $path\nDuration: $duration"
+
+        # Move files shorter than the flag duration
+        if (( duration < flag )); then
+            mkdir -p "$(pwd)/toberemoved"
+            mv "$path" "$(pwd)/toberemoved/"
+        fi
+    fi
 }
-Folder "$(pwd)"
+
+# Start processing from the current directory
+process_folder "$(pwd)"
